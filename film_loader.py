4cdb4dc28038fa4eff81240fe56366a6ffc1615f
Roman Volodin <32717824+RomanAVolodin@users.noreply.github.com>
2021-10-07 15:46:29 +0300

    second file

--- film_loader.py
+++ film_loader.py
@@ -10,13 +10,14 @@ def extract():
     extract data from sql-db
     :return:
     """
-    connection = sqlite3.connect("db.sqlite")
+    connection = sqlite3.connect('db.sqlite')
     cursor = connection.cursor()
 
     # Наверняка это пилится в один sql - запрос, но мне как-то лениво)
 
     # Получаем все поля для индекса, кроме списка актеров и сценаристов, для них только id
-    cursor.execute("""
+    cursor.execute(
+        """
         select id, imdb_rating, genre, title, plot, director,
         -- comma-separated actor_id's
         (
@@ -30,7 +31,8 @@ def extract():
 
         max(writer, writers)
         from movies
-    """)
+    """
+    )
 
     raw_data = cursor.fetchall()
 
@@ -38,10 +40,14 @@ def extract():
     # pprint(cursor.fetchall())
 
     # Нужны для соответсвия идентификатора и человекочитаемого названия
-    actors = {row[0]: row[1] for row in
-              cursor.execute('select * from actors where name != "N/A"')}
-    writers = {row[0]: row[1] for row in
-               cursor.execute('select * from writers where name != "N/A"')}
+    actors = {
+        row[0]: row[1]
+        for row in cursor.execute('select * from actors where name != "N/A"')
+    }
+    writers = {
+        row[0]: row[1]
+        for row in cursor.execute('select * from writers where name != "N/A"')
+    }
 
     return actors, writers, raw_data
 
@@ -57,7 +63,9 @@ def transform(__actors, __writers, __raw_data):
     documents_list = []
     for movie_info in __raw_data:
         # Разыменование списка
-        movie_id, imdb_rating, genre, title, description, director, raw_actors, raw_writers = movie_info
+        movie_id, imdb_rating, genre, title, description, director, raw_actors, raw_writers = (
+            movie_info
+        )
 
         if raw_writers[0] == '[':
             parsed = json.loads(raw_writers)
@@ -65,34 +73,34 @@ def transform(__actors, __writers, __raw_data):
         else:
             new_writers = raw_writers
 
-        writers_list = [(writer_id, __writers.get(writer_id)) for writer_id in
-                        new_writers.split(',')]
-        actors_list = [(actor_id, __actors.get(int(actor_id))) for actor_id in
-                       raw_actors.split(',')]
+        writers_list = [
+            (writer_id, __writers.get(writer_id))
+            for writer_id in new_writers.split(',')
+        ]
+        actors_list = [
+            (actor_id, __actors.get(int(actor_id)))
+            for actor_id in raw_actors.split(',')
+        ]
 
         document = {
-            "_index": "movies",
-            "_id": movie_id,
-            "id": movie_id,
-            "imdb_rating": imdb_rating,
-            "genre": genre.split(', '),
-            "title": title,
-            "description": description,
-            "director": director,
-            "actors": [
-                {
-                    "id": actor[0],
-                    "name": actor[1]
-                }
-                for actor in set(actors_list) if actor[1]
+            '_index': 'movies',
+            '_id': movie_id,
+            'id': movie_id,
+            'imdb_rating': imdb_rating,
+            'genre': genre.split(', '),
+            'title': title,
+            'description': description,
+            'director': director,
+            'actors': [
+                {'id': actor[0], 'name': actor[1]}
+                for actor in set(actors_list)
+                if actor[1]
             ],
-            "writers": [
-                {
-                    "id": writer[0],
-                    "name": writer[1]
-                }
-                for writer in set(writers_list) if writer[1]
-            ]
+            'writers': [
+                {'id': writer[0], 'name': writer[1]}
+                for writer in set(writers_list)
+                if writer[1]
+            ],
         }
 
         for key in document.keys():
@@ -100,13 +108,19 @@ def transform(__actors, __writers, __raw_data):
                 # print('hehe')
                 document[key] = None
 
-        document['actors_names'] = ", ".join(
-            [actor["name"] for actor in document['actors'] if actor]) or None
-        document['writers_names'] = ", ".join(
-            [writer["name"] for writer in document['writers'] if
-             writer]) or None
+        document['actors_names'] = (
+            ', '.join([actor['name'] for actor in document['actors'] if actor])
+            or None
+        )
+        document['writers_names'] = (
+            ', '.join(
+                [writer['name'] for writer in document['writers'] if writer]
+            )
+            or None
+        )
 
         import pprint
+
         pprint.pprint(document)
 
         documents_list.append(document)
